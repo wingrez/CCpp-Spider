d152f882089eade668ae82f7216553d681092ae7
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-05-08 23:45:18 +0800

    20190508

---
+++ domain2ip.cpp
@@ -0,0 +1,19 @@
+/*
+	获取域名的IP地址 
+*/
+
+#include<cstdio>
+#include<winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main(){
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(1,1), &wsaData);
+	
+	char *domain="www.baidu.com";
+	hostent *pHost= gethostbyname(domain);
+	const char *pIp=inet_ntoa(*((struct in_addr *)pHost->h_addr));
+	printf("%s\n", pIp);
+	
+	return 0;
+}
