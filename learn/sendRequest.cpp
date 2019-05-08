93f332e9210494a62631c4096db1679ba9cdd3f5
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-05-09 00:46:27 +0800

    20190509

---
+++ learn/sendRequest.cpp
@@ -0,0 +1,46 @@
+/*
+	发送请求 
+*/
+
+#include<cstdio>
+#include<winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(1,1), &wsaData);
+
+	char *domain="www.baidu.com";
+	hostent *pHost= gethostbyname(domain);
+	const char *pIp=inet_ntoa(*((struct in_addr *)pHost->h_addr));
+	printf("%s\n", pIp);
+
+	SOCKADDR_IN  webServerAddr;
+	webServerAddr.sin_family = AF_INET;
+	webServerAddr.sin_addr.S_un.S_addr=inet_addr(pIp);
+	webServerAddr.sin_port = htons(80);
+
+	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
+	int nRet = connect(sockClient, (struct sockaddr*)&webServerAddr, sizeof(webServerAddr));
+
+	if(nRet<0) {
+		printf("网站连接失败！\n");
+	} else {
+		printf("网站连接成功！\n");
+	}
+
+	char szHttpRest[1024] = {0};
+	sprintf(szHttpRest, "GET https://www.baidu.com/ HTTP/1.1\r\nHost:%s\r\nConnection: Keep-Alive\r\n", domain);
+
+	printf("发送请求：\n");
+	printf("%s\n", szHttpRest);
+
+	nRet = send(sockClient , szHttpRest, strlen(szHttpRest) + 1, 0);
+	if(nRet<0) {
+		printf("请求发送失败！\n");
+	} else {
+		printf("请求发送成功！\n");
+	}
+	
+	return 0;
+}
