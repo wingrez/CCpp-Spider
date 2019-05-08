19470b2ff66b83ae0b4a364d8b6e7e67622ac80e
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-05-09 00:04:00 +0800

    20190509

---
+++ learn/visitWebsite.cpp
@@ -0,0 +1,33 @@
+/*
+	访问网站
+*/
+
+#include<cstdio>
+#include<winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(1,1), &wsaData);
+
+	char *domain="www.baidu.com";
+	hostent *pHost= gethostbyname(domain);
+	const char *pIp=inet_ntoa(*((struct in_addr *)pHost->h_addr));
+	printf("%s\n", pIp);
+
+	SOCKADDR_IN  webServerAddr;
+	webServerAddr.sin_family = AF_INET;
+	webServerAddr.sin_addr.S_un.S_addr=inet_addr(pIp);
+	webServerAddr.sin_port = htons(80);
+
+	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
+	int nRet = connect(sockClient, (struct sockaddr*)&webServerAddr, sizeof(webServerAddr));
+
+	if(nRet<0) {
+		printf("网站连接失败！\n");
+	} else {
+		printf("网站连接成功！\n");
+	}
+
+	return 0;
+}
