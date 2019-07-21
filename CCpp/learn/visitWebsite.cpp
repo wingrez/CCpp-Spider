aa66578e25c98f2e3c539e927df187255821485d
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-07-22 02:38:46 +0800

    完善代码

--- CCpp/learn/visitWebsite.cpp
+++ CCpp/learn/visitWebsite.cpp
@@ -4,13 +4,13 @@
 
 #include<cstdio>
 #include<winsock2.h>
-#pragma comment(lib, "ws2_32.lib")
+//#pragma comment(lib, "ws2_32.lib")
 
 int main() {
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(1,1), &wsaData);
 
-	char *domain="www.baidu.com";
+	const char *domain="www.baidu.com";
 	hostent *pHost= gethostbyname(domain);
 	const char *pIp=inet_ntoa(*((struct in_addr *)pHost->h_addr));
 	printf("%s\n", pIp);
